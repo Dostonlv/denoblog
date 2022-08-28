0a1e0b880b0ca3e7437f2af91aa8377f0984009e
Doston <71603612+salomcha@users.noreply.github.com>
2022-08-28 21:27:30 +0500

    deno blog😼

---
+++ main.ts
@@ -0,0 +1,13 @@
+import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
+
+ blog({
+  title: "Dostonlv",
+  author: "Dostonlv",
+  avatar: "./Itachi.jpg",
+  avatarClass: "rounded-full",
+  links: [
+    { title: "Telegram", url: "https://t.me/Dostonlv" },
+    { title: "GitHub", url: "https://github.com/Dostonlv" },
+  ],
+  background: "#eddddd"
+});
---
+++ posts/hello_world.md
@@ -0,0 +1,6 @@
+---
+title: Hello world!
+publish_date: 2022-08-28
+---
+
+This is my first blog post! This one was done to get github update!
